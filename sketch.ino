8501374417e861f8fbbece823c3f30bfa55c0f8a
Noa Aizen <noa36@users.noreply.github.com>
2026-09-04 13:08:53 +0300

    feat: control LED with button input

--- sketch.ino
+++ sketch.ino
@@ -1,15 +1,21 @@
 const int BUTTON_PIN = 25;
 const int LED_PIN = 26;
 
-void setup() {
+void setup_pins() {
+  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
   pinMode(LED_PIN, OUTPUT);
+}
+
+void update_led(bool isButtonPressed) {
+  digitalWrite(LED_PIN, isButtonPressed ? HIGH : LOW);
+}
+
+void setup() {
+  setup_pins();
   Serial.begin(115200);
-  Serial.println("Hello, ESP32!");
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
+  bool isButtonPressed = digitalRead(BUTTON_PIN) == HIGH;
+  update_led(isButtonPressed);
 }
