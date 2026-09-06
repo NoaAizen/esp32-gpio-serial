a19acce1438a9dbdb7c83ffd584ef2afe1695bb2
Noa Aizen <noa36@users.noreply.github.com>
2026-09-06 15:31:37 +0300

    feat: control LED with button input

--- sketch.ino
+++ sketch.ino
@@ -1,14 +1,21 @@
-const int LED_PIN = 26;
-const int BUTTON_PIN = 25;
+// const int LED_PIN = 26;
 
-// Previous button-based code kept for reference:
-// const int BUTTON_PIN = 25;
-// pinMode(BUTTON_PIN, INPUT_PULLDOWN);
-// if (digitalRead(BUTTON_PIN) == HIGH) {
-//   digitalWrite(LED_PIN, HIGH);
-// } else {
-//   digitalWrite(LED_PIN, LOW);
-// }
+// // void setup() {
+// //   pinMode(LED_PIN, OUTPUT);
+// // }
+
+// // void loop() {
+// //   digitalWrite(LED_PIN, HIGH);
+// //   delay(500);
+
+// //   digitalWrite(LED_PIN, LOW);
+// //   delay(500);
+// // }
+
+
+
+const int BUTTON_PIN = 25;
+const int LED_PIN = 26;
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLDOWN);
@@ -16,5 +23,9 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(LED_PIN, digitalRead(BUTTON_PIN) == HIGH ? HIGH : LOW);
+  if (digitalRead(BUTTON_PIN) == HIGH) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
 }
