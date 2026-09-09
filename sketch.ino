7686a792330a30f3c02bd73181e3aeaac9dde3db
Noa Aizen <noa36@users.noreply.github.com>
2026-09-10 00:10:22 +0300

    feat: add basic LED blink

--- sketch.ino
+++ sketch.ino
@@ -1,46 +1,25 @@
-// const int LED_PIN = 26;
+const int LED_PIN = 26;
 
-// // void setup() {
-// //   pinMode(LED_PIN, OUTPUT);
-// // }
-
-// // void loop() {
-// //   digitalWrite(LED_PIN, HIGH);
-// //   delay(500);
-
-// //   digitalWrite(LED_PIN, LOW);
-// //   delay(500);
-// // }
-
-
-
-// const int BUTTON_PIN = 25;
-// const int LED_PIN = 26;
-
-// void setup() {
-//   pinMode(BUTTON_PIN, INPUT_PULLDOWN);
-//   pinMode(LED_PIN, OUTPUT);
-// }
-
-// void loop() {
-//   if (digitalRead(BUTTON_PIN) == HIGH) {
-//     digitalWrite(LED_PIN, HIGH);
-//   } else {
-//     digitalWrite(LED_PIN, LOW);
-//   }
-// }
-
-
-const int SENSOR_PIN = 34;
-
-void setup() {
-  Serial.begin(115200);
+void setup_pins()
+{
+  pinMode(LED_PIN, OUTPUT);
 }
 
-void loop() {
-  int sensorValue = analogRead(SENSOR_PIN);
-
-  Serial.println(sensorValue);
-
+void update_led()
+{
+  digitalWrite(LED_PIN, HIGH);
   delay(500);
+
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+}
+
+void setup()
+{
+  setup_pins();
+}
+
+void loop()
+{
+  update_led();
 }
