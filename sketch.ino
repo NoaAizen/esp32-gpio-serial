97f99acd355f9596a95e312bdbbd9ed8d4fcfbaf
Noa Aizen <noa36@users.noreply.github.com>
2026-09-10 00:15:42 +0300

    feat: read potentiometer and print to serial

--- sketch.ino
+++ sketch.ino
@@ -25,33 +25,58 @@
 // }
 
 
-const int BUTTON_PIN = 25;
-const int LED_PIN = 26;
+// const int BUTTON_PIN = 25;
+// const int LED_PIN = 26;
+
+// void setup_pins()
+// {
+//   pinMode(BUTTON_PIN, INPUT_PULLDOWN);
+//   pinMode(LED_PIN, OUTPUT);
+// }
+
+// void read_sensor()
+// {
+//   if (digitalRead(BUTTON_PIN) == HIGH)
+//   {
+//     digitalWrite(LED_PIN, HIGH);
+//   }
+//   else
+//   {
+//     digitalWrite(LED_PIN, LOW);
+//   }
+// }
+
+// void setup()
+// {
+//   setup_pins();
+// }
+
+// void loop()
+// {
+//   read_sensor();
+// }
+
+const int SENSOR_PIN = 34;
 
 void setup_pins()
 {
-  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(SENSOR_PIN, INPUT);
 }
 
 void read_sensor()
 {
-  if (digitalRead(BUTTON_PIN) == HIGH)
-  {
-    digitalWrite(LED_PIN, HIGH);
-  }
-  else
-  {
-    digitalWrite(LED_PIN, LOW);
-  }
+  int sensorValue = analogRead(SENSOR_PIN);
+  Serial.println(sensorValue);
 }
 
 void setup()
 {
+  Serial.begin(115200);
   setup_pins();
 }
 
 void loop()
 {
   read_sensor();
+  delay(500);
 }
